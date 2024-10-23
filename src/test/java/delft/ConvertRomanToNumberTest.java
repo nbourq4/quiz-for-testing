b5db5c392b5a0cf896e124485e08ad0258efaeef
Noah Bourque <nbourq4@lsu.edu>
2024-10-23 17:24:01 -0500

    quiz

--- src/test/java/delft/ConvertRomanToNumberTest.java
+++ src/test/java/delft/ConvertRomanToNumberTest.java
@@ -5,12 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 class ConvertRomanToNumberTest {
-
     @Test
-    public void ourFirstTest(){
-
+    public void testConvertStringToNumber() {
+        assertEquals(1, ConvertRomanToNumber.convertStringToNumber("I"));
+        assertEquals(2, ConvertRomanToNumber.convertStringToNumber("II"));
+        assertEquals(4, ConvertRomanToNumber.convertStringToNumber("IV"));
+        assertEquals(5, ConvertRomanToNumber.convertStringToNumber("V"));
     }
 
-
+    @Test
+    public void testAdd() {
+        assertEquals(2, ConvertRomanToNumber.add("I", "I"));
+        assertEquals(6, ConvertRomanToNumber.add("V", "I"));
+        assertEquals(20, ConvertRomanToNumber.add("X", "X"));
+    }
 }
